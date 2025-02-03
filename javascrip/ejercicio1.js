a7dcc735d2a3f51ed3c63e122954bb611bb36fff
Ana Barbera <anabarbera90@gmail.com>
2025-02-03 17:55:46 +0100

    uso de ` y $

--- javascrip/ejercicio1.js
+++ javascrip/ejercicio1.js
@@ -1,5 +1,7 @@
 let a = 13;
 let b = 5;
+let c = 10;
+
 let suma = a + b;
 let resta = a - b;
 let multiplicacion = a * b;
@@ -12,4 +14,10 @@ console.log("multiplicación: "  +multiplicacion);
 console.log("division: " + division);
 console.log("modulo: " + modulo );
 
-console.log("La suma es: " + suma + ". La resta es: " + resta + ". La multiplicación: " + multiplicacion + ". La división: " + division + " y el módulo es: " + modulo)
+console.log("La suma es: " + suma + ". La resta es: " + resta + ". La multiplicación: " + multiplicacion + ". La división: " + division + " y el módulo es: " + modulo);
+
+console.log("La suma de a + b multiplicado por c es: " + suma*c);
+console.log(`La suma de ${a} + ${b} multiplicado por ${c} es: ${suma*c}`);
+
+console.log("El promedio de a, b y c es: " + (a+b+c)/3);
+console.log(`El promedio de ${a}, ${b} y ${c} es:  ${(a+b+c)/3} `);
