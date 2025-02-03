528f5671753539c4d0025bac76c0a74fd3efe314
Ana Barbera <anabarbera90@gmail.com>
2025-02-03 16:55:33 +0100

    ejercicio1

---
+++ javascrip/ejercicio1.js
@@ -0,0 +1,14 @@
+let a = 13;
+let b = 5;
+let suma = a + b;
+let resta = a - b;
+let multiplicacion = a * b;
+let division = a / b;
+let modulo = a % b;
+
+console.log("suma: " + suma);
+console.log("resta: " + resta);
+console.log("multiplicación: "  +multiplicacion);
+console.log("division: " + division);
+console.log("modulo: " + modulo );
+
